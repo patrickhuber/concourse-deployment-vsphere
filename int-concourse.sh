aca995a96e5e176957ba1cc98a3fe87e94019443
Patrick <182398+patrickhuber@users.noreply.github.com>
2018-03-09 11:12:43 -0500

    updates deployment to use git submodules

--- int-concourse.sh
+++ int-concourse.sh
@@ -1,4 +1,12 @@
-bosh interpolate -d concourse \
-     concourse.yml \
-     -v atc_static_ip=192.168.2.0 \
-     --vars-file=no-commit/creds.yml
+bosh int -d concourse \
+     submodules/github.com/concourse/concourse-deployment/cluster/concourse.yml \
+     -l submodules/github.com/concourse/concourse-deployment/versions.yml \
+     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/static-web.yml \
+     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/tls.yml \
+     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/encryption.yml \
+     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/privileged-https.yml \
+     -o ops-files/basic-auth.yml \
+     -o ops-files/atc-tls-variables.yml \
+     -o ops-files/credhub.yml \
+     -l vars-files/lab.yml \
+     -v deployment_name=concourse
