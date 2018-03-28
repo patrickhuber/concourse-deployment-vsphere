89f576fef9af01d0bf97fb3cb012786c0e54e0be
Patrick <182398+patrickhuber@users.noreply.github.com>
2018-03-28 15:16:51 -0400

    adds static ip

--- deploy-concourse.sh
+++ deploy-concourse.sh
@@ -1,6 +1,7 @@
 bosh deploy -d concourse \
      submodules/github.com/concourse/concourse-deployment/cluster/concourse.yml \
      -l submodules/github.com/concourse/concourse-deployment/versions.yml \
+     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/static-web.yml \
      -o ops-files/basic-auth.yml \
      -l vars-files/lab.yml \
      -v deployment_name=concourse
