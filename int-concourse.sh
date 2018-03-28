bae64583d344808982c713c53321257511f474c6
Patrick <182398+patrickhuber@users.noreply.github.com>
2018-03-28 15:05:56 -0400

    'working' 3.9.2 deployment

--- int-concourse.sh
+++ int-concourse.sh
@@ -1,12 +1,7 @@
 bosh int -d concourse \
      submodules/github.com/concourse/concourse-deployment/cluster/concourse.yml \
      -l submodules/github.com/concourse/concourse-deployment/versions.yml \
-     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/static-web.yml \
-     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/tls.yml \
-     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/encryption.yml \
-     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/privileged-https.yml \
      -o ops-files/basic-auth.yml \
-     -o ops-files/atc-tls-variables.yml \
      -o ops-files/credhub.yml \
      -l vars-files/lab.yml \
      -v deployment_name=concourse
