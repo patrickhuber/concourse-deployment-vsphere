788da898e8994e536f0d6787f7805be808c55c89
Patrick <182398+patrickhuber@users.noreply.github.com>
2018-03-28 16:19:58 -0400

    adds encryption

--- deploy-concourse.sh
+++ deploy-concourse.sh
@@ -2,6 +2,7 @@ bosh deploy -d concourse \
      submodules/github.com/concourse/concourse-deployment/cluster/concourse.yml \
      -l submodules/github.com/concourse/concourse-deployment/versions.yml \
      -o submodules/github.com/concourse/concourse-deployment/cluster/operations/static-web.yml \
+     -o submodules/github.com/concourse/concourse-deployment/cluster/operations/encryption.yml \
      -o submodules/github.com/concourse/concourse-deployment/cluster/operations/tls.yml \
      -o submodules/github.com/concourse/concourse-deployment/cluster/operations/privileged-https.yml \
      -o ops-files/basic-auth.yml \
