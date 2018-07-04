e9602b9d3a5e2281cd43e692bb6c4ca466fa8fd2
patrickhuber <patrickhuber@users.noreply.github.com>
2018-07-03 21:24:00 -0400

    removed submodules

--- int-concourse.sh
+++
@@ -1,7 +0,0 @@
-bosh int -d concourse \
-     submodules/github.com/concourse/concourse-deployment/cluster/concourse.yml \
-     -l submodules/github.com/concourse/concourse-deployment/versions.yml \
-     -o ops-files/basic-auth.yml \
-     -o ops-files/credhub.yml \
-     -l vars-files/lab.yml \
-     -v deployment_name=concourse
